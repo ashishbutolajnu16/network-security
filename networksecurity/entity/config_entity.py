30b1aef71cda8d1b2be0e9b903e64785194a9460
ashishbutolajnu16 <ashishbutola7@gmail.com>
2026-04-28 08:52:26 +0530

    Add files via upload

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -1,2 +1,44 @@
 import os
 from datetime import datetime
+
+from networksecurity.constant import training_pipeline
+
+print(training_pipeline.PIPELINE_NAME)
+print(training_pipeline.ARTIFACT_DIR)
+
+class TrainingPipelineconfig:
+    def __init__(self, timestamp=datetime.now()):
+        timestamp = timestamp.strftime("%m_%d_%Y_%H_%M_%S")
+        self.pipeline_name = training_pipeline.PIPELINE_NAME
+        self.artifact_name = training_pipeline.ARTIFACT_DIR
+        self.artifact_dir = os.path.join(self.artifact_name, timestamp)
+        self.timestamp: str = timestamp
+
+class DataIngestionConfig:
+    def __init__(self, training_pipeline_config: TrainingPipelineconfig):
+        self.data_ingestion_dir : str = os.path.join(
+            training_pipeline_config.artifact_dir, training_pipeline.DATA_INGESTION_DIR_NAME
+        )
+        self.feature_store_dir: str = os.path.join(
+            self.data_ingestion_dir,
+            training_pipeline.DATA_INGESTION_FEATURE_STORE_NAME
+        )
+        self.ingested_directory : str = os.path.join(
+            self.data_ingestion_dir,
+            training_pipeline.DATA_INGESTION_INGESTED_DIR
+        )   
+        self.feature_store_file_path : str = os.path.join(
+           self.feature_store_dir,
+           training_pipeline.FILE_NAME
+        )
+        self.train_file_path : str = os.path.join(
+            self.ingested_directory,
+            training_pipeline.TRAIN_FILE_NAME
+        )
+        self.test_file_path : str = os.path.join(
+            self.ingested_directory,
+            training_pipeline.TEST_FILE_NAME
+        )
+        self.collection_name : str = training_pipeline.DATA_INGESTION_COLLECTION_NAME
+        self.database_name : str = training_pipeline.DATA_INGESTION_DATABASE_NAME
+        self.train_test_split_ratio : float = training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
